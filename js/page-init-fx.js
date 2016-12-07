a44510f8741440b2e6b1361e0c4aef204cf3a592
Ben Godfrey <BenJetson@users.noreply.github.com>
2016-12-07 09:30:39 -0500

    Initial squash commit for 12/7.

--- js/page-init-fx.js
+++ js/page-init-fx.js
@@ -1,9 +1,16 @@
 var initFunctions = {
     desktopUnCollapse: function() {
-        $(document).ready(function(){
-            if ($(window).width() >= 1024) {
-                $('.panel-collapse:not(.navbar-collapse)').collapse("show");
-            }
-        });
+        if ($(window).width() >= 1024) {
+            $('.panel-collapse:not(.navbar-collapse)').collapse("show");
+        }
+    },
+    waves: function() {
+        Waves.init();
+    },
+    collapse: function() {
+        $('.collapse').collapse("hide");
+    },
+    dotdotdot: function() {
+        $(".ellipsis").dotdotdot();
     }
 }
